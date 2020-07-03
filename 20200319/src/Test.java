c0c4c2793f5748d46ef778abcec875996b6848c9
sjc <1508445842@qq.com>
2020-07-03 17:02:50 +0800

    1

---
+++ 20200319/src/Test.java
@@ -0,0 +1,13 @@
+import java.util.PriorityQueue;
+
+/*
+JDK：提供PriorityQueue接口的使用
+注意：三点
+优先级队列底层的数据结构--》使用的是堆
+注意：new出的空间在堆 优先级队列底层结构也是堆 此处的两个堆不是一个概念
+     new-->堆：指的是一块具有特殊作业的内存空间
+     优先级队列底层的堆：值得是一种数据结构
+ */
+public class Test {
+
+}
